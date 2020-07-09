ae9eb30f1bceec65a3bc929efdd7bad7c51b9661
Olaide Abdulazeez <dj3plles@gmail.com>
2020-07-09 11:12:14 +0100

    Applied styles

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import {Provider} from "react-redux";
+import React from "react";
+import thunkMiddleware from "redux-thunk";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+import '../styles/global.css';
+
+
+const reducers = combineReducers({
+
+});
+
+const store = process.env.environment === 'dev' ? createStore(reducers, composeWithDevTools(applyMiddleware(thunkMiddleware))) : createStore(reducers, applyMiddleware(thunkMiddleware));
+
+export default function App({Component, pageProps}) {
+    return <Provider store={store}><Component {...pageProps} /></Provider>
+}
