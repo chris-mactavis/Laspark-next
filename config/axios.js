59d2b014cfb838409692938ee47887215f1672e7
Olaide Abdulazeez <dj3plles@gmail.com>
2020-07-28 17:58:49 +0100

    fix axios

--- config/axios.js
+++ config/axios.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const axiosInstance = axios.create({
-    baseURL: process.env.environment !== 'dev' ? 'http://laspark.test/api/v1/' : 'http://159.203.67.70/api/v1/',
+    baseURL: process.env.environment === 'dev' ? 'http://laspark.test/api/v1/' : 'http://159.203.67.70/api/v1/',
     headers: {
         'Content-Type': 'application/json',
     }
