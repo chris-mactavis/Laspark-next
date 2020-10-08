38c56a7025d72b3231606f5b79e9b5acb04bf062
Olaide Abdulazeez <dj3plles@gmail.com>
2020-10-08 12:18:56 +0100

    Payment Invoice

---
+++ pages/invoice/index.js
@@ -0,0 +1,124 @@
+import Head from "next/head";
+import Layout from "../../Components/Layout";
+import React, {useState, useEffect} from "react";
+import axiosInstance from "../../config/axios";
+import {randomString} from "../../Utils/String";
+import {toggleParkRules} from "../../store/actions/booking";
+
+const Invoice = ({billNumber, invoice}) => {
+    console.log(billNumber, invoice);
+    const [transactionId, setTransactionId] = useState(randomString(20));
+    const [stringHash, setStringHash] = useState(null);
+    console.log(stringHash);
+    const payHandler = () => {
+        const hashString = `${process.env.REVPAY_TOKEN}LASPARK${billNumber}${transactionId}${invoice.amount}` + "http://165.227.73.31/verify-payment";
+        setStringHash(
+            CryptoJS.MD5(hashString).toString().toUpperCase()
+        )
+        // return;
+        // localStorage.setItem('bookedPark', JSON.stringify({date: data.date, spaceId: parkSpace.id}));
+
+        document.getElementById('frm').submit();
+    }
+
+    return <Layout hasHeader={false}>
+        <Head>
+            <title>Payment Invoice | Laspark</title>
+        </Head>
+
+        <section className="profile">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h2 className="text-center mb-5">Payment Invoice</h2>
+                    </div>
+                </div>
+
+                <div className="row booking-history">
+                    <div className="col">
+                        <table role="table">
+                            <thead role="rowgroup">
+                            <tr role="row">
+                                <th role="columnheader">Item</th>
+                                <th role="columnheader">Value</th>
+                            </tr>
+                            </thead>
+                            <tbody role="rowgroup">
+                            <tr role="row" className="cursor-pointer">
+                                <td role="cell">Invoice Number</td>
+                                <td role="cell">#{invoice.ref}</td>
+                            </tr>
+                            <tr role="row" className="cursor-pointer">
+                                <td role="cell">Bill To</td>
+                                <td role="cell">{invoice.user.first_name} {invoice.user.last_name}</td>
+                            </tr>
+                            <tr role="row" className="cursor-pointer">
+                                <td role="cell">Item Description</td>
+                                <td role="cell">{invoice.is_service ? invoice.service.service : invoice.park_space.parks_garden.name}</td>
+                            </tr>
+                            <tr role="row" className="cursor-pointer">
+                                <td role="cell">Amount</td>
+                                <td role="cell">₦{Intl.NumberFormat().format(invoice.amount)}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col text-center d-flex flex-column">
+                        <div>
+                            <button type="button" onClick={payHandler} className="btn extra-thin green-transparent">Pay Now
+                            </button>
+                        </div>
+                        <small className="mt-2">Payment is non refundable!</small>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {/*<form name="frm" id="frm2" method="post" target="_parent"*/}
+        {/*      action="https://test.qpay.ng:7071/PaymentGateway/Index">*/}
+        {/*    <input type="hidden" name="type" value="Webguid"/>*/}
+        {/*    <input type="hidden" name="transactionId" value={transactionId}/>*/}
+        {/*    <input type="hidden" name="billReference" value={billNumber}/>*/}
+        {/*    <input type="hidden" name="amount" value={invoice.amount}/>*/}
+        {/*    <input type="hidden" name="returnUrl" value="http://165.227.73.31/verify-payment"/>*/}
+        {/*    <input type="hidden" name="clientCode" value="LASPARK"/>*/}
+        {/*    <input type="hidden" name="Hash" value={stringHash}/>*/}
+        {/*</form>*/}
+
+        <form name="frm" id="frm" method="post" target="_parent"
+              action="https://test.qpay.ng:7071/PaymentGateway/Index">
+            <input type="hidden" name="type" value="Webguid"/>
+            <input type="hidden" name="transactionId" value="wd7tzzvq9y000000wd7t"/>
+            <input type="hidden" name="billReference" value="LASPARK-1602155126"/>
+            <input type="hidden" name="amount" value="3000"/>
+            <input type="hidden" name="returnUrl" value="http://165.227.73.31/verify-payment"/>
+            <input type="hidden" name="clientCode" value="LASPARK"/>
+            <input type="hidden" name="Hash" value="5DD5EE4CD61C3809E9E86275D4CF7ED7"/>
+        </form>
+    </Layout>
+}
+
+Invoice.getInitialProps = async ({query, req}) => {
+    const token = req.cookies.token;
+    try {
+        const {data: {invoice, bill_number: billNumber}} = await axiosInstance.get(`invoices/${query.invoice}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return {
+            billNumber,
+            invoice
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            billNumber: null,
+            invoice: null
+        }
+    }
+}
+
+export default Invoice;
---
+++ pages/profile/park-bookings/[id].js
@@ -0,0 +1,136 @@
+import axiosInstance from "../../../config/axios";
+import Layout from "../../../Components/Layout";
+import Head from "next/head";
+import React, {useEffect, useState} from "react";
+import {useForm} from "react-hook-form";
+import {useDispatch} from "react-redux";
+import {loader} from "../../../store/actions/loader";
+import Token from "../../../Utils/Token";
+import {showNotifier} from "../../../store/actions/notifier";
+
+export default function ServiceBookingDetail({booking, messages: parkMessages, spaceBookingId}) {
+
+    const createMarkup = (content) => ({__html: content});
+    const {register, handleSubmit, reset} = useForm();
+    const dispatch = useDispatch();
+    const [messages, setMessages] = useState(parkMessages || []);
+    console.log(booking);
+    useEffect(() => {
+        async function fetchData() {
+            const {data: {messages}} = await axiosInstance.get(`my-booked-spaces/${spaceBookingId}`);
+            setMessages(messages);
+        }
+        fetchData();
+    }, [])
+
+    const replyHandler = async data => {
+        dispatch(loader());
+        const formData = new FormData();
+        formData.append('message', data.message);
+        Array.from(data.attachments).forEach(attachment => formData.append('attachments[]', attachment));
+        try {
+            const {data: response} = await axiosInstance.post('park-space-bookings-messages/' + booking.id, formData, {
+                headers: {Authorization: `Bearer ${Token()}`}
+            });
+            console.log(response);
+            dispatch(loader());
+            setMessages(message => [response.data, ...message]);
+            reset();
+            dispatch(showNotifier('Reply sent!'));
+        } catch (e) {
+            console.log(e);
+            dispatch(loader());
+            dispatch(showNotifier(e.response.data.message, 'danger'));
+        }
+        console.log(data);
+    }
+
+    return <Layout hasHeader={false}>
+        <Head>
+            <title>Park Booking Detail | Laspark</title>
+        </Head>
+
+        <section className="profile single-service-booking">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4 className="text-center mb-0">#{booking.order_number} ({booking.park.name})</h4>
+                        <h5 className="text-center mb-5">{booking.park_space.space}</h5>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-8 mx-auto reply-container">
+                        <div>
+                            <h5>Reply</h5>
+                        </div>
+
+                        <div className="form">
+                            <form className="account-create p-0" onSubmit={handleSubmit(replyHandler)}>
+                                <textarea ref={register} name="message" placeholder="Compose your message..."/>
+
+                                <input ref={register} name="attachments" type="file" multiple/>
+
+                                <button className="btn green thin wide mx-auto" type="submit">Reply</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                {
+                    messages.map(message => <div key={message.id} className="row">
+                        <div className="col-8 mx-auto chats">
+                            <div className="header d-flex flex-column">
+                                <p className="mb-0">{message.user ? message.user.full_name : 'N/A'}</p>
+                                <span>{message.user ? (message.user.role === 'admin' ? 'Staff' : 'Me') : 'N/A'}</span>
+                            </div>
+                            <div className="body">
+                                {(message.message && message.message !== 'undefined') && <div dangerouslySetInnerHTML={createMarkup(message.message)}/>}
+                                {
+                                    message.attachments_decoded.length > 0 && <div className="attachment-section">
+                                        <div className="d-flex flex-column mt-3">
+                                            <a href="#" className="mb-1 text-dark">Attachment</a>
+                                            {
+                                                message.attachments_decoded.map((attachment, i) => <span key={i}
+                                                                                                         className="d-block mb-2">
+                                                <a target="_blank" className="text-dark"
+                                                   href={attachment}>{attachment}</a>
+                                            </span>)
+                                            }
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+        </section>
+    </Layout>
+}
+
+export async function getStaticPaths(context) {
+    const {data: response} = await axiosInstance.get('park-booking-slug');
+    const ids = response.map(id => ({
+        params: {
+            id: id.id.toString()
+        }
+    }));
+    return {
+        paths: ids,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    const id = context.params.id;
+    const {data: {data: booking, messages}} = await axiosInstance.get(`my-booked-spaces/${id}`);
+    // console.log(data);
+    return {
+        props: {
+            booking,
+            messages,
+            spaceBookingId: id
+        }
+    }
+}
