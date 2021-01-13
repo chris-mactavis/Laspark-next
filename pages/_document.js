fcd456dd47d612b50c9753221621329a3d4a33c2
khrisbreezy <khristopherabolarin@gmail.com>
2021-01-13 13:44:31 +0100

    Added a document.js file

---
+++ pages/_document.js
@@ -0,0 +1,60 @@
+import Document, {Html, Head, Main, NextScript } from 'next/document';
+
+class document extends Document  {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                <meta charSet="utf-8"/>
+
+
+                <title>Laspark</title>
+                <meta name="description" content=""/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16"
+                      href="/images/favicon/favicon-16x16.png"/>
+                <link rel="stylesheet"
+                      href="/css/bootstrap.min.css"/>
+
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
+
+
+                <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
+                <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
+
+                {/*<meta*/}
+                {/*    name="description"*/}
+                {/*    content="Learn how to build a personal website using Next.js"*/}
+                {/*/>*/}
+                {/*<meta*/}
+                {/*    property="og:image"*/}
+                {/*    content={`https://og-image.now.sh/${encodeURI(*/}
+                {/*        siteTitle*/}
+                {/*    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}*/}
+                {/*/>*/}
+                {/*<meta name="og:title" content={siteTitle} />*/}
+                {/*{meta}*/}
+
+                <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+                        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+                        crossOrigin="anonymous"/>
+                <script type="text/javascript" src="/slick/slick.min.js"/>
+                <script src="/js/bootstrap.min.js"/>
+                <script src="/js/crypto-js.min.js"/>
+                <script src="/js/main.js"/>
+            </Head>
+    
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+    
+};
+
+export default document;
