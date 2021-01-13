5e1945df87ff734eb35ef9659184383b8bf7fbaf
khrisbreezy <khristopherabolarin@gmail.com>
2021-01-13 17:19:10 +0100

    added the document and removed jquery and bootsrap npm uninstall

--- pages/_document.js
+++ pages/_document.js
@@ -5,27 +5,38 @@ class document extends Document  {
         return (
             <Html lang="en">
                 <Head>
-                    <meta charSet="utf-8" />
+                <meta charSet="utf-8"/>
 
-                    <meta name="description" content="" />
+                <meta name="description" content=""/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                    <link rel="stylesheet" href="/css/bootstrap.min.css" />
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"></link>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16"
+                      href="/images/favicon/favicon-16x16.png"/>
+                <link rel="stylesheet"
+                      href="/css/bootstrap.min.css"/>
 
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
-
-                    <link rel="stylesheet" type="text/css" href="/slick/slick.css" />
-                    <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
 
 
+                <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
+                <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
 
-                    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDWajwZf_MGn2k5vVYECfrtphdGTCAKurg&libraries=places"></script>
-                    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+                {/*<meta*/}
+                {/*    name="description"*/}
+                {/*    content="Learn how to build a personal website using Next.js"*/}
+                {/*/>*/}
+                {/*<meta*/}
+                {/*    property="og:image"*/}
+                {/*    content={`https://og-image.now.sh/${encodeURI(*/}
+                {/*        siteTitle*/}
+                {/*    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}*/}
+                {/*/>*/}
+                {/*<meta name="og:title" content={siteTitle} />*/}
+                {/*{meta}*/}
+
+                <script src="https://code.jquery.com/jquery-3.4.1.min.js"
                         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
                         crossOrigin="anonymous" />
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
