29542d23e41631bce869a60fa7085188e819734a
khrisbreezy <khristopherabolarin@gmail.com>
2021-01-13 15:36:59 +0100

    coment the nprogress css stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ class document extends Document  {
                 <link rel="stylesheet"
                       href="/css/bootstrap.min.css"/>
 
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
+                {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" /> */}
 
 
                 <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
