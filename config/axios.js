70770afae79bd063f84300af2bb1ef8f09665472
Olaide Abdulazeez <dj3plles@gmail.com>
2021-04-09 12:28:52 +0100

    revert back to the old backend

--- config/axios.js
+++ config/axios.js
@@ -2,9 +2,11 @@ const axios = require('axios');
 
 const axiosInstance = axios.create({
     // baseURL: process.env.environment === 'dev' ? 'http://laspark.test/api/v1/' : 'http://159.203.67.70/api/v1/',
-    baseURL: 'http://161.35.7.233/api/v1/',
+    baseURL: 'http://159.203.67.70/api/v1/',
     headers: {
         'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': true
     }
 });
 
