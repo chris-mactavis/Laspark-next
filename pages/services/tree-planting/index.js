340ef5d2cf08f2948dd60340baaf870f9288547b
Olaide Abdulazeez <dj3plles@gmail.com>
2021-04-20 14:09:54 +0100

    removed items from tree planting dashboard

--- pages/services/tree-planting/index.js
+++ pages/services/tree-planting/index.js
@@ -100,39 +100,12 @@ const TreePlanting = ({localGovernment}) => {
                         <form className="account-create w-100" onSubmit={handleSubmit(serviceRequestHandler)}>
                             <h1>Tree Planting</h1>
 
-                            <input ref={register({required: 'This field is required'})} type="text" name="tree_type"
-                                   placeholder="Tree type*"/>
-                            {errors.tree_type && <Error>{errors.tree_type.message}</Error>}
-
-                            <div className="text-left">
-                                <label className="w-100">Attach Tree Picture*</label>
-                                <input ref={register({required: 'This field is required'})} type="file" multiple
-                                       name="tree_pictures" placeholder="Tree Pictures*"/>
-                                {errors.tree_pictures && <Error>{errors.tree_pictures.message}</Error>}
-                            </div>
-
                             <div className="d-flex inline-form">
                                 <label>Date for Planting</label>
                                 <input ref={register({required: 'This field is required'})} type="date"
                                        name="date_for_planting" min={minDate}
                                        placeholder="Proposed date for planting*"/>
                             </div>
-
-                            <select ref={register({required: 'This field is required'})} name="local_government_id">
-                                <option value="">Select Local Government</option>
-                                {
-                                    localGovernment.map(lg => <option value={lg.id} key={lg.id}>{lg.name}</option>)
-                                }
-                            </select>
-                            {errors.local_government_id && <Error>{errors.local_government_id.message}</Error>}
-
-                            <input ref={register({required: 'This field is required'})} type="text" name="street_name"
-                                   placeholder="Street name*"/>
-                            {errors.street_name && <Error>{errors.street_name.message}</Error>}
-
-                            <input ref={register({required: 'This field is required'})} type="text" name="house_number"
-                                   placeholder="House Number*"/>
-                            {errors.house_number && <Error>{errors.house_number.message}</Error>}
 
                             <input ref={register({required: 'This field is required'})} type="number" min="0" name="no_of_trees"
                                    id="cname" placeholder="Number of trees to be planted*"/>
@@ -142,9 +115,13 @@ const TreePlanting = ({localGovernment}) => {
                                    placeholder="Purpose for Planting*"/>
                             {errors.purpose && <Error>{errors.purpose.message}</Error>}
 
-                            <textarea ref={register} rows="3" name="attach_letter"
+                            <textarea ref={register({required: 'This field is required'})} rows="3" name="location"
+                                      placeholder="Location*"/>
+                            {errors.location && <Error>{errors.location.message}</Error>}
+
+                            <textarea ref={register} rows="3" name="request_letter"
                                       placeholder="Request letter (minimum of 250 words)"/>
-                            {errors.attach_letter && <Error>{errors.attach_letter.message}</Error>}
+                            {errors.request_letter && <Error>{errors.request_letter.message}</Error>}
 
                             <button className="btn green thin wide" type="submit">Submit Request</button>
                         </form>
