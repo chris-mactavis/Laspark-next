0090a09da37a68f4e8584c7278d165eff9a81147
khrisbreezy <63780922+chris-mactavis@users.noreply.github.com>
2021-04-22 13:08:25 +0100

    Updated the signup form address field to display error when required

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -99,7 +99,8 @@ const Signup = () => {
                                 {errors.phone && <Error>{errors.phone.message}</Error>}
 
                                 <input type="text" ref={register({required: 'This field is required'})} name="address"
-                                       id="address" placeholder="Address"/>
+                                       id="address" placeholder="Address*"/>
+                                {errors.address && <Error>{errors.address.message}</Error>}
 
                                 <input type="password" ref={register({required: 'This field is required', pattern: {value: /^[A-Za-z]+[A-Za-z]/i, message: "password must have an upper case"}, minLength: { value: 8, message: "password must be more than 8 characters" }})}
                                        name="password" id="pwd"
